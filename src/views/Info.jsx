07cc7ce7a4d29f8fb66fe59c43ea3821f0ef4c38
potionstory <potionstory@lfpartners.co.kr>
2019-07-30 17:17:48 +0900

    page head add

--- src/views/Info.jsx
+++ src/views/Info.jsx
@@ -10,11 +10,17 @@ class Info extends Component {
   render() {
     return (
       <Layout>
-        <h2>INFO PAGE</h2>
-        <article className="contents">
-          <h3>INFO</h3>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-        </article>
+        <section className="section_info">
+          <div className="headline">
+            <div className="headline-image"></div>
+            <h2><span className="font-gradient">FE-star?<br />FE-star!</span></h2>
+            <p><span className="font-gradient">사람과 사람사이를<br />연결시켜 주는것</span></p>
+          </div>
+          <article className="contents">
+            <h3>INFO</h3>
+            <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
+          </article>
+        </section>
       </Layout>
     );
   };
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,43 +1,28 @@
 import React, { Component } from 'react';
 import Layout from './Layout';
+// import HeadImg from '../images/main/img_main_head.png';
 
 class Main extends Component {
 
   componentDidMount() {
+    // console.log(HeadImg);
     window.scrollTo(0, 0);
   };
 
   render() {
     return (
       <Layout>
-        <h2>MAIN PAGE</h2>
-        <article className="contents">
-          <h3>MAIN</h3>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-          <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
-        </article>
+        <section className="section_main">
+          <div className="headline">
+            <div className="headline-image"><img src='../images/main/img_main_head.png' /></div>
+            <h2><span className="font-gradient">FE-star?<br />FE-star!</span></h2>
+            <p><span className="font-gradient">사람과 사람사이를<br />연결시켜 주는것</span></p>
+          </div>
+          <article className="contents">
+            <h3>MAIN</h3>
+            <p>가나다라마바사아자차카타파하<br />ABCEDFGHIJKLMNOPQRSTUVWXYZ<br />0123456789</p>
+          </article>
+        </section>
       </Layout>
     );
   };
