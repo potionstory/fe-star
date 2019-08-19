9a7534827bc763685d1a3c7d6244a3d4f346d553
Potion Story <potionstory@gmail.com>
2019-08-20 01:10:22 +0900

    ScheduleBox styling

--- src/components/ScheduleBox/ScheduleBox.jsx
+++ src/components/ScheduleBox/ScheduleBox.jsx
@@ -22,35 +22,35 @@ class ScheduleBox extends Component {
                 </thead>
                 <tbody>
                   <tr>
-                    <td>19:00 - 19:55</td>
+                    <td className="number">19:00 - 19:55</td>
                     <td>저녁식사(1부)</td>
                   </tr>
                   <tr>
-                    <td>19:55 - 20:05</td>
+                  <td className="number">19:55 - 20:05</td>
                     <td>자리 바꾸기</td>
                   </tr>
                   <tr>
-                    <td>20:05 - 21:00</td>
+                    <td className="number">20:05 - 21:00</td>
                     <td>저녁식사(2부)</td>
                   </tr>
                   <tr>
-                    <td>21:00 - 21:10</td>
+                    <td className="number">21:00 - 21:10</td>
                     <td>쉬는시간</td>
                   </tr>
                   <tr>
-                    <td>21:10 - 21:40</td>
+                    <td className="number">21:10 - 21:40</td>
                     <td>빙고게임</td>
                   </tr>
                   <tr>
-                    <td>21:40 - 22:10</td>
+                    <td className="number">21:40 - 22:10</td>
                     <td>행운권 추첨</td>
                   </tr>
                   <tr>
-                    <td>22:10 - 22:40</td>
+                    <td className="number">22:10 - 22:40</td>
                     <td>팀 게임</td>
                   </tr>
                   <tr>
-                    <td>22:40 - 23:00</td>
+                    <td className="number">22:40 - 23:00</td>
                     <td>청소 및 마무리</td>
                   </tr>
                 </tbody>
