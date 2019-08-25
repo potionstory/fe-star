4fd5845a51490b81d5fe589025694a55fd16d494
Potion Story <potionstory@gmail.com>
2019-08-26 00:51:04 +0900

    JoinCard styling

--- src/components/JoinCard/JoinCard.jsx
+++ src/components/JoinCard/JoinCard.jsx
@@ -1,35 +1,65 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle, faGrinStars, faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 import './JoinCard.scss';
 
 class JoinCard extends Component {
+
+  state = {
+    item : [
+      {
+        name: '카톡방 대화명',
+        focus: false,
+      },
+      {
+        name: '이름',
+        focus: false,
+      },
+      {
+        name: '전화번호',
+        focus: false,
+      },
+      {
+        name: '메일주소',
+        focus: false,
+      }
+    ]
+  };
+
+  handleFocus = (index) => {
+    const { item } = this.state;
+    this.setState({
+      item: [
+        ...item.slice(0, index),
+        {
+          name: item[index].name,
+          focus: !item[index].focus
+        },
+        ...item.slice(index + 1, item.length)
+      ]
+    });
+  };
+
   render() {
+
+    const { item } = this.state;
+    const { handleFocus } = this;
+    
     return (
       <>
         <div className="join-wrap">
           <div className="join-card">
-            <div className="join-item">
-              <span className="title">카톡방 대화명</span>
-              <div className="item-box">
-                <label htmlFor="01"><FontAwesomeIcon icon={faUserCircle} size="2x" /></label>
-                <input type="text" id="01" name="nickname" placeholder="" />
-              </div>
-            </div>
-            <div className="join-item">
-              <span className="title">카톡방 대화명</span>
-              <div className="item-box">
-                <label htmlFor="01"><FontAwesomeIcon icon={faUserCircle} size="2x" /></label>
-                <input type="text" id="01" name="nickname" placeholder="" />
-              </div>
-            </div>
-            <div className="join-item">
-              <span className="title">카톡방 대화명</span>
-              <div className="item-box">
-                <label htmlFor="01"><FontAwesomeIcon icon={faUserCircle} size="2x" /></label>
-                <input type="text" id="01" name="nickname" placeholder="" />
-              </div>
-            </div>
+            {item.map((value, index) => {
+              return (
+                <div key={value.name + index} className={value.focus ? "join-item active" : "join-item"}>
+                  <span className="title">{value.name}</span>
+                  <div className="item-box">
+                    <label htmlFor="01"><FontAwesomeIcon icon={faUserCircle} size="2x" /></label>
+                    <input type="text" id="01" name="nickname" placeholder="" onFocus={() => handleFocus(index)} onBlur={() => handleFocus(index)} />
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
       </>
