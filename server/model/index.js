55c54991132d53894822fffe81786ccb3fba149f
Potion Story <potionstory@gmail.com>
2019-09-05 01:52:46 +0900

    JoinCard styling

--- server/model/index.js
+++ server/model/index.js
@@ -4,8 +4,8 @@ const { Schema } = mongoose;
 const MemberSchema = new Schema({
   name: { type: String, require: true },
   nickname: { type: String, require: true },
-  password: { type: String, require: true },
-  mobile: { type: String, require: true }
+  mobile: { type: String, require: true },
+  email: { type: String, require: true }
 }, { collection: 'member' });
 
 const Member = mongoose.model('Member', MemberSchema);
--- src/components/JoinCard/JoinCard.jsx
+++ src/components/JoinCard/JoinCard.jsx
@@ -8,19 +8,27 @@ class JoinCard extends Component {
   state = {
     item : [
       {
-        name: '카톡방 대화명',
+        title: '카톡방 대화명', 
+        name: 'nickname',
+        value: '',
         focus: false,
       },
       {
-        name: '이름',
+        title: '이름',
+        name: 'name',
+        value: '',
         focus: false,
       },
       {
-        name: '전화번호',
+        title: '전화번호 ( - 없이 입력해주세요)',
+        name: 'mobile',
+        value: '',
         focus: false,
       },
       {
-        name: '메일주소',
+        title: '메일주소',
+        name: 'email',
+        value: '',
         focus: false,
       }
     ]
@@ -32,7 +40,9 @@ class JoinCard extends Component {
       item: [
         ...item.slice(0, index),
         {
+          title: item[index].title,
           name: item[index].name,
+          value: item[index].value,
           focus: !item[index].focus
         },
         ...item.slice(index + 1, item.length)
@@ -40,11 +50,35 @@ class JoinCard extends Component {
     });
   };
 
+  handleChange = (index, e) => {
+    const { item } = this.state;
+    const { value } = e.target;
+    this.setState({
+      item: [
+        ...item.slice(0, index),
+        {
+          ...item[index],
+          value
+        },
+        ...item.slice(index + 1, item.length)
+      ]
+    });
+  };
+
+  handleJoin = () => {
+    console.log('handleJoin');
+  };
+
   render() {
 
+    let active = 0;
     const { item } = this.state;
-    const { handleFocus } = this;
+    const { handleFocus, handleChange, handleJoin } = this;
     
+    item.map(n => {
+      active += n.value ? 1 : 0;
+    });
+
     return (
       <>
         <div className="join-wrap">
@@ -52,14 +86,26 @@ class JoinCard extends Component {
             {item.map((value, index) => {
               return (
                 <div key={value.name + index} className={value.focus ? "join-item active" : "join-item"}>
-                  <span className="title">{value.name}</span>
+                  <span className="title">{value.title}</span>
                   <div className="item-box">
-                    <label htmlFor="01"><FontAwesomeIcon icon={faUserCircle} size="2x" /></label>
-                    <input type="text" id="01" name="nickname" placeholder="" onFocus={() => handleFocus(index)} onBlur={() => handleFocus(index)} />
+                    <label htmlFor={value.name + index}><FontAwesomeIcon icon={faUserCircle} size="2x" /></label>
+                    <input
+                      type={value.name !== "mobile" ? "text" : "number"}
+                      id={value.name + index}
+                      className={value.name === "mobile" ? "number" : null}
+                      name={value.name}
+                      placeholder=""
+                      onFocus={() => handleFocus(index)}
+                      onBlur={() => handleFocus(index)}
+                      onChange={(e) => handleChange(index, e)}
+                    />
                   </div>
                 </div>
               )
             })}
+          </div>
+          <div className="join-submit">
+            <button type="button" className={active === item.length ? "active" : null}onClick={handleJoin}>참가하기</button>
           </div>
         </div>
       </>
