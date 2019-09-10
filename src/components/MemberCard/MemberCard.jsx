e53a9ff66715cb5d32ffafbd4569a0d54e3d5c04
potionstory <potionstory@lfpartners.co.kr>
2019-09-10 17:56:15 +0900

    MemberCard styling

--- src/components/MemberCard/MemberCard.jsx
+++ src/components/MemberCard/MemberCard.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAsterisk } from '@fortawesome/free-solid-svg-icons';
 import './MemberCard.scss';
 
 const MemberCard = () => {
   return (
     <>
-      MemberCard 
+      <div className="member-wrap">
+        <div className="member-card">
+          <div className="card-head">
+            <FontAwesomeIcon icon={faAsterisk} size="2x" />
+            <h3>참가 확정</h3>
+          </div>
+          <div className="card-body">
+            확정 인원
+          </div>
+        </div>
+        <div className="member-card">
+          <div className="card-head">
+            <FontAwesomeIcon icon={faAsterisk} size="2x" />
+            <h3>참가 대기</h3>
+          </div>
+          <div className="card-body">
+            대기 인원
+          </div>
+        </div>
+      </div>
     </>
   );
 };
